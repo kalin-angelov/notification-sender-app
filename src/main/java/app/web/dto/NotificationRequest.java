142e012cef608e85227f07a28c7f6ae8e00afebc
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-06 16:35:16 +0300

    create: create dto for notification request

---
+++ src/main/java/app/web/dto/NotificationRequest.java
@@ -0,0 +1,19 @@
+package app.web.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class NotificationRequest {
+
+    @NotNull
+    private UUID userId;
+
+    @NotNull
+    private String subject;
+
+    @NotNull
+    private String body;
+}
