4c4ae9a35f2380b04c45a8b781a39e21ee9ae433
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-06 16:36:49 +0300

    edit: add new method

--- src/main/java/app/servise/NotificationService.java
+++ src/main/java/app/servise/NotificationService.java
@@ -1,12 +1,18 @@
 package app.servise;
 
 import app.exceptions.UserNotificationSettingNotFound;
+import app.model.EmailNotification;
 import app.model.NotificationSetting;
-import app.repository.NotificationRepository;
+import app.model.NotificationStatus;
+import app.model.NotificationType;
+import app.repository.EmailNotificationRepository;
+import app.repository.NotificationSettingRepository;
 import app.web.dto.NotificationPreferenceRequest;
+import app.web.dto.NotificationRequest;
 import app.web.mapper.DtoMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,12 +23,19 @@ import java.util.UUID;
 @Service
 public class NotificationService {
 
-    @Autowired
-    private NotificationRepository notificationRepository;
+    private final EmailNotificationRepository emailNotificationRepository;
+    private final NotificationSettingRepository notificationSettingRepository;
+    private final MailSender mailSender;
 
-    public NotificationSetting upsertNotification(NotificationPreferenceRequest dto) {
+    public NotificationService(EmailNotificationRepository notificationRepository, NotificationSettingRepository notificationSettingRepository, MailSender mailSender) {
+        this.emailNotificationRepository = notificationRepository;
+        this.notificationSettingRepository = notificationSettingRepository;
+        this.mailSender = mailSender;
+    }
 
-        Optional<NotificationSetting> optionalUserNotification = notificationRepository.findByUserId(dto.getUserId());
+    public NotificationSetting upsertNotificationSetting(NotificationPreferenceRequest dto) {
+
+        Optional<NotificationSetting> optionalUserNotification = notificationSettingRepository.findByUserId(dto.getUserId());
 
         if (optionalUserNotification.isPresent()) {
             NotificationSetting notification = optionalUserNotification.get();
@@ -30,7 +43,7 @@ public class NotificationService {
             notification.setEnabled(dto.isNotificationEnabled());
             notification.setContactInfo(dto.getContactInfo());
             notification.setUpdatedOn(LocalDateTime.now());
-            return notificationRepository.save(notification);
+            return notificationSettingRepository.save(notification);
         }
 
         NotificationSetting notification = NotificationSetting.builder()
@@ -42,12 +55,46 @@ public class NotificationService {
                 .updatedOn(LocalDateTime.now())
                 .build();
 
-        return notificationRepository.save(notification);
+        return notificationSettingRepository.save(notification);
     }
 
-    public NotificationSetting getUserNotification(UUID userId) {
-        NotificationSetting notificationSetting = notificationRepository.findByUserId(userId).orElseThrow(UserNotificationSettingNotFound::new);
+    public NotificationSetting getUserNotificationSetting(UUID userId) {
+        NotificationSetting notificationSetting = notificationSettingRepository.findByUserId(userId).orElseThrow(UserNotificationSettingNotFound::new);
         log.info("Notification setting for user with id [%s] was not found.".formatted(userId));
         return notificationSetting;
+    }
+
+    public EmailNotification sendNotification(NotificationRequest notificationRequest) {
+
+        UUID userId = notificationRequest.getUserId();
+        NotificationSetting userNotificationSetting = getUserNotificationSetting(userId);
+
+        if (!userNotificationSetting.isEnabled()) {
+            throw new IllegalArgumentException("User with id [%s] don't want to receive notification".formatted(userId));
+        }
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(userNotificationSetting.getContactInfo());
+        message.setSubject(notificationRequest.getSubject());
+        message.setText(notificationRequest.getBody());
+
+        EmailNotification notification = EmailNotification.builder()
+                .subject(notificationRequest.getSubject())
+                .body(notificationRequest.getBody())
+                .createdOn(LocalDateTime.now())
+                .type(NotificationType.EMAIL)
+                .userId(userId)
+                .isDeleted(false)
+                .build();
+
+        try {
+            mailSender.send(message);
+            notification.setStatus(NotificationStatus.SUCCEEDED);
+        } catch (Exception exception) {
+            notification.setStatus(NotificationStatus.FAILED);
+            log.info("There was an issue sending an email to [%s] due to {%s}".formatted(userNotificationSetting.getContactInfo(), exception.getMessage()));
+        }
+
+        return emailNotificationRepository.save(notification);
     }
 }
