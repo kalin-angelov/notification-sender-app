96f9ecf7364b3ce2f69c70e16bb0a63bb679e58a
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-07 12:06:30 +0300

    edit: add query

--- src/main/java/app/repository/EmailNotificationRepository.java
+++ src/main/java/app/repository/EmailNotificationRepository.java
@@ -2,10 +2,18 @@ package app.repository;
 
 import app.model.EmailNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EmailNotificationRepository extends JpaRepository<EmailNotification, UUID> {
+
+    @Query("""
+            SELECT n FROM EmailNotification n WHERE n.userId = :userId AND n.deleted = false
+            """)
+    List<EmailNotification> findAllByUserIdAndDeleteIsFalse(@Param("userId") UUID userId);
 }
