25fbb2dc774d6a4edd99a91a991a7f7fe51b6c00
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-15 14:51:41 +0300

    edit: change the message of the exception

--- src/main/java/app/web/ExceptionAdvice.java
+++ src/main/java/app/web/ExceptionAdvice.java
@@ -1,6 +1,6 @@
 package app.web;
 
-import app.exceptions.UserNotificationSettingNotFound;
+import app.exceptions.UserNotificationInformationNotFound;
 import app.web.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler(UserNotificationSettingNotFound.class)
+    @ExceptionHandler(UserNotificationInformationNotFound.class)
     public ResponseEntity<ErrorResponse> handleUserNotificationSettingsNotFound() {
 
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Notification settings not found.");
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Notification information not found.");
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
