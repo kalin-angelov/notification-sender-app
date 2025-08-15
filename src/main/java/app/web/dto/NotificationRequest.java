6ae8652a88475fc27261ee99f5c08fd8aa314a7c
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-15 14:55:34 +0300

    edit: add Builder annotation

--- src/main/java/app/web/dto/NotificationRequest.java
+++ src/main/java/app/web/dto/NotificationRequest.java
@@ -1,11 +1,13 @@
 package app.web.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@Builder
 public class NotificationRequest {
 
     @NotNull
