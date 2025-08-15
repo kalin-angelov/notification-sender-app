e907a53e0dbf0bc3d52b708c81882db8e3421424
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-15 14:54:44 +0300

    edit: add more random objects for test purposes

--- src/test/java/app/TestBuilder.java
+++ src/test/java/app/TestBuilder.java
@@ -1,11 +1,16 @@
 package app;
 
+import app.model.EmailNotification;
 import app.model.NotificationSetting;
+import app.model.NotificationStatus;
 import app.model.NotificationType;
 import app.web.dto.NotificationPreferenceRequest;
+import app.web.dto.NotificationRequest;
+import app.web.dto.NotificationResponse;
 import app.web.dto.NotificationTypeRequest;
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @UtilityClass
@@ -30,6 +35,38 @@ public class TestBuilder {
                 .contactInfo("SendMyNotificationHere@gmail.com")
                 .type(NotificationTypeRequest.EMAIL)
                 .notificationEnabled(true)
+                .build();
+    }
+
+    public static NotificationResponse aRandomNotificationResponse() {
+
+        return NotificationResponse.builder()
+                .subject("This is my subject")
+                .status(NotificationStatus.SUCCEEDED)
+                .createdOn(LocalDateTime.now())
+                .type(NotificationType.EMAIL)
+                .build();
+    }
+
+    public static EmailNotification aRandomEmailNotification() {
+
+        return EmailNotification.builder()
+                .userId(userId)
+                .subject("This is my subject")
+                .body("This is my body")
+                .createdOn(LocalDateTime.now())
+                .type(NotificationType.EMAIL)
+                .status(NotificationStatus.SUCCEEDED)
+                .isDeleted(false)
+                .build();
+    }
+
+    public static NotificationRequest aRandomNotificationRequest() {
+
+        return NotificationRequest.builder()
+                .userId(userId)
+                .subject("This is my subject")
+                .body("This is my body")
                 .build();
     }
 }
