48edb9c881a32841179e54a6f952929a7d6732cb
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-15 14:57:22 +0300

    edit: ...

--- src/main/java/app/exceptions/UserNotificationSettingNotFound.java
+++
@@ -1,9 +0,0 @@
-package app.exceptions;
-
-public class UserNotificationSettingNotFound extends RuntimeException {
-    public UserNotificationSettingNotFound(String message) {
-        super(message);
-    }
-
-    public UserNotificationSettingNotFound() {}
-}
