1600d870e97c90f54ab3ac58ead1bbd49a47a272
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-15 14:51:04 +0300

    edit: change the query to the DB for filtering the notification by userId and if the notification is deleted

--- src/main/java/app/repository/EmailNotificationRepository.java
+++ src/main/java/app/repository/EmailNotificationRepository.java
@@ -2,18 +2,14 @@ package app.repository;
 
 import app.model.EmailNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EmailNotificationRepository extends JpaRepository<EmailNotification, UUID> {
 
-    @Query("""
-            SELECT n FROM EmailNotification n WHERE n.userId = :userId AND n.deleted = false
-            """)
-    List<EmailNotification> findAllByUserIdAndDeleteIsFalse(@Param("userId") UUID userId);
+    Optional<List<EmailNotification>> findAllByUserIdAndIsDeleted(UUID userId, boolean isDeleted);
 }
